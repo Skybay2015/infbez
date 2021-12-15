19caadfac7c7af08ccb272f9f744913e579350c4
Fedir Tsarenko <fedya132465@gmail.com>
2021-12-16 01:30:35 +0200

    finished

---
+++ laba9/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace laba9
+{
+    class Program
+    {
+        public static byte[] ComputeHashSha512(byte[] toBeHashed)
+        {
+            using (var sha512 = SHA512.Create())
+            {
+                return sha512.ComputeHash(toBeHashed);
+            }
+        }
+
+        private readonly static string CspContainerName = "RsaKeyContainer";
+        public void AssignNewKey()
+        {
+            CspParameters cspParameters = new CspParameters(1)
+            {
+                KeyContainerName = CspContainerName,
+
+                ProviderName = "Microsoft Strong Cryptographic Provider"
+            };
+            var rsa = new RSACryptoServiceProvider(cspParameters)
+            {
+                PersistKeyInCsp = true
+            };
+            File.WriteAllText("file.xml", rsa.ToXmlString(false));
+        }
+
+        public byte[] SignData(byte[] hashOfDataToSign)
+        {
+
+            var cspParams = new CspParameters
+            {
+                KeyContainerName = CspContainerName,
+            };
+
+            using (var rsa = new RSACryptoServiceProvider(cspParams))
+            {
+                rsa.PersistKeyInCsp = true;
+                var rsaFormatter = new RSAPKCS1SignatureFormatter(rsa);
+                rsaFormatter.SetHashAlgorithm("SHA512");
+                return rsaFormatter.CreateSignature(hashOfDataToSign);
+            }
+        }
+        public bool VerifySignature(byte[] hashOfDataToSign, byte[] signature)
+        {
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.FromXmlString(File.ReadAllText("file.xml"));
+                var rsaDeformatter = new RSAPKCS1SignatureDeformatter(rsa);
+                rsaDeformatter.SetHashAlgorithm("SHA512");
+                return rsaDeformatter.VerifySignature(hashOfDataToSign, signature);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var doc = Encoding.UTF8.GetBytes("abcd1234");
+            byte[] hashedDocument = ComputeHashSha512(doc);
+            var digitalSignature = new Program();
+            digitalSignature.AssignNewKey();
+            var signature = digitalSignature.SignData(hashedDocument);
+            var verified = digitalSignature.VerifySignature(hashedDocument, signature);
+
+            Console.WriteLine(" Original Text = " + Encoding.Default.GetString(doc));
+            Console.WriteLine();
+            Console.WriteLine(" Digital Signature = " + Convert.ToBase64String(signature));
+            if (verified)
+            {
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Wrong");
+            }
+        }
+
+    }
+}
