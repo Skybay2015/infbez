db749b03ebea70e1f5090e945bcaaa336fd72c63
Fedir Tsarenko <fedya132465@gmail.com>
2021-12-23 10:41:52 +0200

    finished

---
+++ Exam/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random(123);
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(random.Next(1, 50));
+            }
+        }
+    }
+}
